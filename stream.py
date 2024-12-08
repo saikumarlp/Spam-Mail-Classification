9a64224eeda4884250895592a5ba5268b1bc5063
saikumarlp <154747239+saikumarlp@users.noreply.github.com>
2024-12-08 19:11:22 +0530

    Add files via upload

---
+++ stream.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pickle
+import sklearn
+model = pickle.load(open('spam.pkl','rb'))
+cv = pickle.load(open('vec.pkl','rb'))
+
+def main():
+    st.title("Email Spam Classification Application")
+    st.write("This is a Machine Learning application to classify emails as spam or ham.. ")
+    st.subheader("Classification")
+    user_input = st.text_area("Enter an Email Classify",height=150)
+    if st.button("Classify"):
+        if user_input:
+            data = [user_input]
+            print(data)
+            vec = cv.transform(data).toarray()
+            result = model.predict(vec)
+            if result[0] == 0:
+                st.success("This is Not A Spam Email")
+            else:
+                st.error("Caution!!!" "This is A Spam Mail")
+        else:
+            st.write("Please Enter an Email to Classify..")
+main()
